21875dff9a3d988284876a2485a35b6a284672a8
Zuhair Abbas <syedzuhairabbas02@gmail.com>
2026-04-08 19:48:13 +0500

    feat: GET songs API route connected to MongoDB Atlas and added My database URL

---
+++ app/api/songs/route.js
@@ -0,0 +1,25 @@
+import client from "@/lib/mongodb";
+
+export async function POST(req) {
+  const body = await req.json();
+
+  await client.connect();
+
+  const db = client.db("FrozenBeats");
+  const songs = db.collection("songs");
+
+  const result = await songs.insertOne(body);
+
+  return Response.json(result);
+}
+
+export async function GET(req){
+    await client.connect();
+
+    const db = client.db("FrozenBeats");
+    const songs  = db.collection("songs");
+
+    const result = await songs.find().toArray();
+
+    return Response.json(result);
+}
