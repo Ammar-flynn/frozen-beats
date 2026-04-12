02f4f2874d4c4275b228e4b39874c54b075babc7
Zuhair Abbas <syedzuhairabbas02@gmail.com>
2026-04-13 00:00:30 +0500

    feat: play count increment route

---
+++ app/api/songs/play/route.js
@@ -0,0 +1,16 @@
+import client from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export async function POST (req){
+    const body = await req.json();
+
+    await client.connect();
+    const db = client.db("FrozenBeats");
+    const songs = db.collection("songs");
+
+    const { songId } = body;
+
+    const result = await songs.updateOne({_id : new ObjectId(songId)} , {$inc: {plays : 1}});
+
+    return Response.json(result);
+}
